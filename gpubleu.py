0ee22f67f8285b419d977ba3cb7d5948838d2ad4
Raphael Shu <raphael@uaca.com>
2017-04-26 13:37:18 +0900

    Wed Apr 26 13:37:18 JST 2017

--- gpubleu.py
+++ gpubleu.py
@@ -31,75 +31,84 @@ class GPUBLEUEvaluator(object):
         # Raw graph
         self._graph = theano.function([g, r, m1, m2, m3, m4, nx, nr], bleu_matrix)
     
-    def _get_counts(self, seqs, cmap=None):
-        if cmap is None:
-            cmap = [{}, {}, {}, {}]
-        cnt_list = []
+    def _convert_counts(self, seqs, cmap):
+        sizes = map(len, cmap)
+        offset_2 = sizes[0]
+        offset_3 = sum(sizes[:2])
+        offset_4 = sum(sizes[:3])
+        matrix = np.zeros((len(seqs), sum(sizes)), dtype=FLOATX)
+        for n, seq in enumerate(seqs):
+            for i in range(len(seq)):
+                if seq[i] in cmap[0]:
+                    id = cmap[0][seq[i]]
+                    matrix[n, id] += 1.
+                if i >= 1:
+                    t = tuple(seq[i - 1:i + 1])
+                    if t in cmap[1]:
+                        id = cmap[1][t]
+                        matrix[n, offset_2 + id] += 1.
+                if i >= 2:
+                    t = tuple(seq[i - 2:i + 1])
+                    if t in cmap[2]:
+                        id = cmap[2][t]
+                        matrix[n, offset_3 + id] += 1.
+                if i >= 3:
+                    t = tuple(seq[i - 3:i + 1])
+                    if t in cmap[3]:
+                        id = cmap[3][t]
+                        matrix[n, offset_4 + id] += 1.
+        return matrix
+    
+    def precompute_ngram_map(self, seqs):
+        cmap = [{}, {}, {}, {}]
         for seq in seqs:
-            cnt = [{}, {}, {}, {}]
             for i in range(len(seq)):
                 if seq[i] not in cmap[0]:
                     cmap[0][seq[i]] = len(cmap[0])
-                id = cmap[0][seq[i]]
-                if id not in cnt[0]:
-                    cnt[0][id] = 0
-                cnt[0][id] += 1
                 if i >= 1:
                     t = tuple(seq[i - 1:i + 1])
                     if t not in cmap[1]:
                         cmap[1][t] = len(cmap[1])
-                    id = cmap[1][t]
-                    if id not in cnt[1]:
-                        cnt[1][id] = 0
-                    cnt[1][id] += 1
                 if i >= 2:
                     t = tuple(seq[i - 2:i + 1])
                     if t not in cmap[2]:
                         cmap[2][t] = len(cmap[2])
-                    id = cmap[2][t]
-                    if id not in cnt[2]:
-                        cnt[2][id] = 0
-                    cnt[2][id] += 1
                 if i >= 3:
                     t = tuple(seq[i - 3:i + 1])
                     if t not in cmap[3]:
                         cmap[3][t] = len(cmap[3])
-                    id = cmap[3][t]
-                    if id not in cnt[3]:
-                        cnt[3][id] = 0
-                    cnt[3][id] += 1
-            cnt_list.append(cnt)
-        return cnt_list, cmap
-    
-    def _convert_counts(self, cnt_list, cmap):
-        cnt_vecs = []
-        sizes = map(len, cmap)
-        sum_size = sum(sizes)
-        for cnt in cnt_list:
-            vec = np.zeros(sum_size, dtype="int32")
-            base_idx = 0
-            for i in range(4):
-                for id, c in cnt[i].items():
-                    vec[id + base_idx] = c
-                base_idx += sizes[i]
-            cnt_vecs.append(vec)
-        matrix = np.stack(cnt_vecs)
-        return matrix.astype(FLOATX), sizes
-    
-    def evaluate(self, hyps, refs):
+        return cmap
+        
+    def evaluate(self, hyps, refs, ngram_map=None):
+        if ngram_map is None:
+            if len(hyps) < len(refs):
+                ngram_map = self.precompute_ngram_map(hyps)
+            else:
+                ngram_map = self.precompute_ngram_map(refs)
+        seps = map(len, ngram_map)
         tok_lens = np.array(map(len, hyps), dtype=FLOATX)
         ev_lens = np.array(map(len, refs), dtype=FLOATX)
-        ev_cnt_list, cmap = self._get_counts(refs)
-        cnt_list, cmap = self._get_counts(hyps, cmap=cmap)
-        ev_matrix, seps = self._convert_counts(ev_cnt_list, cmap)
-        matrix, seps = self._convert_counts(cnt_list, cmap)
+
+        matrix = self._convert_counts(hyps, ngram_map)
+        ev_matrix = self._convert_counts(refs, ngram_map)
+        
+        
         bleu_matrix = self._graph(*([matrix, ev_matrix] + seps + [tok_lens, ev_lens]))
         return bleu_matrix
 
 
 if __name__ == '__main__':
+    import sys
     # Test code
     bleu = GPUBLEUEvaluator()
-    sents = [[1, 2, 3, 4, 5, 3, 4, 1], [2, 2, 2, 5, 7], [1, 2, 3, 3, 4, 5, 1, 5, 6, 7, 4, 3, 3, 6]]
+    sents = [[1, 2, 3, 4, 5, 3, 4, 1], [2, 2, 2, 5, 7], [1, 2, 3, 3, 4, 5, 1, 5, 6, 7, 4, 3, 3, 6], [1, 2, 3, 4, 5], [1, 2, 3, 7, 7, 7]] * 1000
     ref = [[1, 2, 3, 4, 5, 7]]
-    print (bleu.evaluate(sents, ref))
+    K = 1000
+    
+    print ("evaluate {} hyps with {} refs for {} times".format(len(sents), len(ref), K))
+    
+    cmap = bleu.precompute_ngram_map(ref)
+    for _ in range(K):
+         bleu.evaluate(sents, ref, ngram_map=cmap)
+         sys.stdout.write(".")
+         sys.stdout.flush()
